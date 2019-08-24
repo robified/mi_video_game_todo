57cc35d03f945ca71e44bb9e0f2c9ad961425058
Robin <daorob@gmail.com>
2019-08-24 17:34:13 -0500

    Define auth-related routes.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: 'index',
+    failureRedirect: 'index'
+  }
+));
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('index');
 });
 
 module.exports = router;
