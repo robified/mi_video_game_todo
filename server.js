7d6133cf662444514da330794299a031f20d9fd3
Robin <daorob@gmail.com>
2019-08-28 14:54:22 -0500

    Require and use videogamesRouter. Require passportCtlr.

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var passport = require('passport');
+var passportCtrl = require('./controllers/passport/passport')
 
 require('dotenv').config();
 require('./config/database');
 require('./config/passport');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var videogamesRouter = require('./routes/videogames');
 
 
 var app = express();
@@ -34,7 +35,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/videogames', passportCtrl.isLoggedIn, videogamesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
