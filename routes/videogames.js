72423d021474ba817a14356cf3d44cdda5a0c321
Robin <daorob@gmail.com>
2019-08-28 22:09:25 -0500

    Create videogamesCtrl create/show/edit/delete fn.

--- routes/videogames.js
+++ routes/videogames.js
@@ -4,7 +4,14 @@ var videogamesCtrl = require('../controllers/videogames/videogames');
 
 router.get('/', videogamesCtrl.index);
 router.get('/new', videogamesCtrl.new);
+router.get('/:id', videogamesCtrl.show);
+router.get('/:id/edit', videogamesCtrl.edit);
+
 
 router.post('/', videogamesCtrl.create);
+router.delete('/:id', videogamesCtrl.delete);
+
+
+router.put('/:id');
 
 module.exports = router;
