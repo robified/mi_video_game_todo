bc6a26a793ea2a1b1fad0806e273bc3f25967981
Robin <daorob@gmail.com>
2019-08-28 22:11:36 -0500

    Separate videogame and videocomments schema.

--- models/user.js
+++ models/user.js
@@ -2,31 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
-var videocommentsSchema = new Schema({
-    comment: String
-}, {
-    timestamps: true
-});
-
-
-var videogameSchema = new Schema({
-    title: String,
-    statusOfCompletion: {
-      type: String,
-      enum: ['Not Started', 'In Progress', 'Completed']
-    },
-    comments: [videocommentsSchema]
-
-  }, {
-    timestamps: true
-  });
-
-
 var userSchema = new Schema({
     name: String,
     googleId: String,
-    photo: String,
-    videogames: [videogameSchema]
+    photo: String
 }, {
     timestamps: true
 });
