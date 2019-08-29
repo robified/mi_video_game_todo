8867c76ca24808698db8c2be9b73d96b38f7fbc7
Robin <daorob@gmail.com>
2019-08-29 10:22:40 -0500

    Add/require commentsRouter.

--- routes/videogames.js
+++ routes/videogames.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var videogamesCtrl = require('../controllers/videogames/videogames');
+var commentsRouter = require('./comments');
+
+
+router.use('/', commentsRouter);
+
 
 router.get('/', videogamesCtrl.index);
 router.get('/new', videogamesCtrl.new);
 router.get('/:id', videogamesCtrl.show);
 router.get('/:id/edit', videogamesCtrl.edit);
+
 
 router.post('/', videogamesCtrl.create);
 router.put('/:id', videogamesCtrl.update);
