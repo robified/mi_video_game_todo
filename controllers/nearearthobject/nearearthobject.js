2026d79838e488d8dcc472733e70819254fb6a75
Robin <daorob@gmail.com>
2019-08-30 03:45:36 -0500

    Deploy to Heroku

--- controllers/nearearthobject/nearearthobject.js
+++ controllers/nearearthobject/nearearthobject.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 var request = require('request'); 
+var comet = process.env.COMET;
 
 const rootURL = 'https://api.nasa.gov/neo/rest/v1/feed?api_key='
 
@@ -7,12 +9,11 @@ module.exports = {
 };
 
 function index(req, res, next) {
-    request(rootURL + process.env.COMET, function(err, response, comet) {
+    request(rootURL + comet, function(err, response, comet) {
         var cometData = JSON.parse(comet);
-        var asteroids = cometData.near_earth_objects;
-        var theDay = asteroids['2019-09-05'][0].nasa_jpl_url + ';old=0;orb=1;cov=0;log=0;cad=0#orb';
-        // console.log(firstDateComet);
-        console.log(theDay);
+        var asteroids = cometData.near_earth_objects
+        var firstDate = Object.keys(asteroids)[0]
+        var theDay = asteroids[firstDate][0].nasa_jpl_url + ';old=0;orb=1;cov=0;log=0;cad=0#orb';
         res.render('nearearthobject/index', {
             theDay,
             user: req.user
