950bb5c620046edb6daa21e26fb6518a2baba493
Robin <daorob@gmail.com>
2019-08-30 03:03:26 -0500

    Update index/style and index fx.

--- controllers/nearearthobject/nearearthobject.js
+++ controllers/nearearthobject/nearearthobject.js
@@ -1,5 +1,4 @@
 var request = require('request'); 
-var comet = process.env.COMET;
 
 const rootURL = 'https://api.nasa.gov/neo/rest/v1/feed?api_key='
 
@@ -8,11 +7,11 @@ module.exports = {
 };
 
 function index(req, res, next) {
-    request(rootURL + comet, function(err, response, comet) {
+    request(rootURL + process.env.COMET, function(err, response, comet) {
         var cometData = JSON.parse(comet);
         var asteroids = cometData.near_earth_objects
-        var firstDate = Object.keys(asteroids)[0]
-        var theDay = asteroids[firstDate][0].nasa_jpl_url + ';old=0;orb=1;cov=0;log=0;cad=0#orb';
+        var firstAsteroidDate = Object.keys(asteroids)[0]
+        var theDay = asteroids[firstAsteroidDate][0].nasa_jpl_url + ';old=0;orb=1;cov=0;log=0;cad=0#orb';
         res.render('nearearthobject/index', {
             theDay,
             user: req.user
