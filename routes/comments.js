c4118abe9d8a17f3578710469acc15155c841bfe
Robin <daorob@gmail.com>
2019-08-30 04:10:27 -0500

    Full functioning application.

--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var commentsCtrl = require('../controllers/comments/comments');
 
-
 router.post('/:gameid/comments', commentsCtrl.create);
 
 module.exports = router;
